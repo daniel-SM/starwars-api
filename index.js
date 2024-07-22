c9691e3e517e8785a13ab74b48be1fddc8d9c6d5
Daniel Santos <danieldsm.on@gmail.com>
2024-07-21 23:50:31 -0300

    :sparkles: feat: create mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require("express")
-const app = express()
+const mongoose = require("mongoose")
+
 const port = 3000
+const app = express()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URL)
+
+const Movie = mongoose.model("Movie", {
+    title: String,
+    description: String,
+    image_url: String,
+    trailer_url: String,
+})
 
 app.get("/", (req, res) => {
     return res.send("Hello World!")
+})
+
+app.post("/", async (req, res) => {
+    const movie = new Movie({
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url,
+    })
+
+    await movie.save()
+
+    return res.send(movie)
 })
 
 app.listen(port, () => {
